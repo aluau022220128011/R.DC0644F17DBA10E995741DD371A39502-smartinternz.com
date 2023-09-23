5623b67734af741b86120873e28a021ff11128af
aluau022220128011 <144459147+aluau022220128011@users.noreply.github.com>
2023-09-23 13:12:50 +0530

    Add files via upload

---
+++ main (5).py
@@ -0,0 +1,12 @@
+def linearsearchproduct(productlist,targetproduct):
+  indices=[]
+  for(index,product in enumrate(productlist)):
+  if product==targetproduct:
+  indices.append(index)
+  return indices
+  product ={"open","bag","pen","pencil"}
+  target="pen"
+  target="apple"
+  result=linearsearchproduct(product,target)
+  
+  
